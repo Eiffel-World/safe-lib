36ef911e0cbf7d215bf180900a12f5506ed9d049
efa <efa>
2001-09-26 10:54:44 +0000

    use GOBO test now

---
+++ base_types/conversion/test/format/test.bat
@@ -0,0 +1,10 @@
+getest getest-win.ise
+
+pause
+
+
+rem
+rem Copyright: 2001, Eric Fafchamps, <eric.fafchamps@belgacom.net>
+rem Released under the Eiffel Forum License <www.eiffel-forum.org>
+rem See file <forum.txt>
+rem
