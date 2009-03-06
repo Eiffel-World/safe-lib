9bb66e60bb3f81449bd9fd18edd10b9081d1dfa2
pgcrism <pgcrism>
2009-03-06 12:34:53 +0000

    Support of Gobo Eiffel

---
+++ ecurses/spec/ge/linux/epanel.h
@@ -0,0 +1,13 @@
+/* 
+ * Include file for ecurses, the eiffel library to (N)Curses
+ *
+ * Released under the Eiffel Forum Licence, or the LGPL
+ * 
+ * Author : Paul G. Crismer (pgcrism@ibm.net)
+ * 
+ * $Version: $
+ * $Date: 2009/03/06 12:34:54 $
+ * 
+*/
+#include <panel.h>
+
